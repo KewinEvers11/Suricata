dd40bac5fab0d0cb9004ccab0c739940830c12d6
KewinEvers11 <kewin4@gmail.com>
2026-08-30 16:42:21 -0500

    Init 01. Add Graddle initial configuration for project

---
+++ src/test/java/org/oyabun/suricata/SuricataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oyabun.suricata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuricataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
