5bf73e3bf0844a178a02fa7c7afd7483ea60b5bb
Kewin Evers Yagari <88694037+KewinEvers11@users.noreply.github.com>
2026-08-30 20:12:40 -0500

    Feature/init 01 (#1)
    * Init 01. Add Graddle initial configuration for project
    
    * INIT_01. Add postgresql driver, and application-develop to run app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Suricata
 API REST para monitorear precios de productos.
+
+# Technologies:
+* **Api REST:** Spring Boot 4.1.1
+* **Database:** Postgresql 18.6
---
+++ src/main/java/org/oyabun/suricata/SuricataApplication.java
@@ -0,0 +1,13 @@
+package org.oyabun.suricata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuricataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuricataApplication.class, args);
+	}
+
+}
