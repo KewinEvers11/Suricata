ed65840f40da9cf857ca91b480f106313daaeb44
KewinEvers11 <kewin4@gmail.com>
2026-09-10 08:19:59 -0500

    Add flyway support and add first migration

---
+++ src/main/resources/db/migration/V1.01__initial-entities-added.sql
@@ -0,0 +1,53 @@
+CREATE TABLE solicitud_monitoreo (
+    id UUID PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    url_producto VARCHAR(255),
+    nombre_usuario VARCHAR(255),
+    revisor VARCHAR(255),
+    estado VARCHAR(50) NOT NULL,
+    create_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE producto_monitoreado (
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
+    nombre VARCHAR(255) NOT NULL,
+    marca VARCHAR(127) NOT NULL,
+    modelo VARCHAR(127) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE sitio (
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
+    nombre VARCHAR(255) NOT NULL,
+    base_url VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE producto_sitio (
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
+    ultimo_precio DOUBLE PRECISION,
+    producto_id UUID,
+    sitio_id UUID,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE registro_precio_producto (
+    tiempo_registro TIMESTAMP WITH TIME ZONE NOT NULL,
+    precio DOUBLE PRECISION,
+    producto_sitio_id UUID
+);
+
+ALTER TABLE producto_sitio ADD CONSTRAINT fk_producto_monitoreado_id
+    FOREIGN KEY (producto_id) REFERENCES producto_monitoreado(id);
+
+ALTER TABLE producto_sitio ADD CONSTRAINT fk_sitio_id
+    FOREIGN KEY (sitio_id) REFERENCES sitio(id);
+
+ALTER TABLE registro_precio_producto ADD CONSTRAINT fk_producto_sitio
+    FOREIGN KEY (producto_sitio_id) REFERENCES producto_sitio(id);
+
+
